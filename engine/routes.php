a36183e4edbb7f3ca28befea3ec3ead7751c554c
THEM0R <the.mor.ua@gmail.com>
2020-03-19 19:24:30 +0200

    0.0.21

--- engine/routes.php
+++ engine/routes.php
@@ -13,10 +13,14 @@ Router::add('admin', 'admin:index','main');
  * category
  */
 Router::add('admin/category', 'admin:category','category');
+Router::add('admin/category/add', 'admin:categoryAdd','categoryAdd');
+Router::add('admin/category/(id:int)/edit', 'admin:categoryEdit','categoryEdit');
+Router::add('admin/category/(id:int)/delete', 'admin:categoryDelete','categoryDelete');
 /*
  * articles
  */
 Router::add('admin/articles', 'admin:articles','articles');
+Router::add('admin/articles/add', 'admin:articlesAdd','articlesAdd');
 Router::add('admin/articles/(id:int)', 'admin:articlesView','articlesView');
 Router::add('admin/articles/(id:int)/edit', 'admin:articlesEdit','articlesEdit');
 Router::add('admin/articles/(id:int)/delete', 'admin:articlesDelete','articlesDelete');
@@ -24,8 +28,8 @@ Router::add('admin/articles/(id:int)/delete', 'admin:articlesDelete','articlesDe
  * page
  */
 Router::add('admin/page', 'admin:page','page');
+Router::add('admin/page/add', 'admin:pageAdd','pageAdd');
 Router::add('admin/page/(id:int)', 'admin:pageView','pageView');
-Router::add('admin/page/(id:int)/add', 'admin:pageAdd','pageAdd');
 Router::add('admin/page/(id:int)/edit', 'admin:pageEdit','pageEdit');
 Router::add('admin/page/(id:int)/delete', 'admin:pageDelete','pageDelete');
 /*
