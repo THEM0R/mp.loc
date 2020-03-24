dae42df88db78a5bccdc8b33c90dfedd432787f8
THEM0R <the.mor.ua@gmail.com>
2020-03-24 16:53:32 +0200

    0.1.12

--- public/admin/themor/js/image.upload.js
+++ public/admin/themor/js/image.upload.js
@@ -2,6 +2,130 @@
 
 $(function () {
 
+  function imageUpload(method) {
+
+    // action
+    modal.preview
+      .hide()
+      .html('');
+
+    modal.lg
+      .hide();
+
+    // upload input
+    modal.button_file.change(function () {
+
+      console.log('2: change button_file');
+
+      var poster = this.files[0];
+      var format = poster.name.split('.').pop().toLowerCase();
+      if (!isValid(format)) {
+        $(this).val('');
+      } else {
+        var reader = new FileReader();
+        reader.onload = function (e) {
+          bindCroppie(e.target.result);
+        };
+        reader.readAsDataURL(poster);
+      }
+    });
+    // upload url
+    modal.button_url.change(function () {
+
+      console.log('2: change button_url');
+
+      var url = $(this).val();
+
+      var format = url.split('.').pop().toLowerCase();
+
+      if (!isValid(format)) {
+        $(this).val('');
+      } else {
+        $.ajax({
+          type: 'POST',
+          url: '/upload/url',
+          data: {
+            url: url
+          },
+          dataType: 'json',
+          success: function (response) {
+            if (response.type != 'error') {
+              bindCroppie(response.data);
+            }
+          }
+        });
+
+        $(this).val('');
+      }
+
+    });
+    // ajax result
+    modal.button_croppie_upload.click(function () {
+
+      console.log('4: click button_croppie_upload');
+
+      modal.mcroppie.croppie('result', {
+
+        type: 'base64',
+        size: 'viewport',
+        format: 'jpeg'
+
+      }).then(function (response) {
+
+        // view
+
+        console.log(method);
+
+        method(response);
+
+        // modal
+        modal.button_file.val('');
+
+        modal.mcroppie.children('div').children('img').attr('src', '');
+
+        modal.selector
+          .removeClass('modal-lg')
+          .addClass('modal-sm');
+
+        modal.sm.show();
+        modal.lg.hide();
+
+        modal.preview.html('');
+
+        modal.mcroppie.hide();
+
+        modal.mcroppie.croppie('destroy');
+
+        modal.modals.modal('hide');
+      });
+
+
+
+    });
+
+    // others
+
+    // modal.mcroppie.croppie({
+    //
+    //   enableExif: true,
+    //   viewport: {
+    //     width: 300,
+    //     height: 250,
+    //     type: 'canvas'
+    //   },
+    //   boundary: {
+    //     width: 766,
+    //     height: 450
+    //   }
+    // });
+
+    //croppies({w: 300, h: 250}, {w: 700, h: 400});
+    // croppies(modal.mcroppie);
+
+    croppies({w: 300, h: 250}, {w: 700, h: 400});
+
+  }
+
   // action
   view.link.click(function (event) {
 
@@ -10,8 +134,6 @@ $(function () {
     console.log('1: poster click link');
 
     imageUpload(addPoster);
-
-    croppies({w: 300, h: 250}, {w: 700, h: 400});
 
     modal.modals
       .children('.modal-dialog')
